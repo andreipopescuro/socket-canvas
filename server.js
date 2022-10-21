0064f518639d8b8a5427c123c886d10f7ada46a9
andreipopescuro <91088680+andreipopescuro@users.noreply.github.com>
2022-10-21 16:44:52 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 let express = require("express");
-// Create the app
 let app = express();
 const socket = require("socket.io");
-// Set up the server
-// process.env.PORT is related to deploying on heroku
+
 let server = app.listen(3000);
 
 app.use(express.static("public"));
@@ -15,23 +13,15 @@ const io = socket(server, {
   },
 });
 
-// WebSocket Portion
-// WebSockets work with the HTTP server
 
-// Register a callback function to run when we have an individual connection
-// This is run for each individual user that connects
 io.sockets.on(
   "connection",
-  // We are given a websocket object in our function
   function (socket) {
     console.log("We have a new client: " + socket.id);
 
-    // When this user emits, client side: socket.emit('otherevent',some data);
     socket.on("mouse", function (data) {
-      // Send it to all other clients
       socket.broadcast.emit("mouse", data);
 
-      // io.sockets.emit('message', "this goes to everyone");
     });
 
     socket.on("disconnect", function () {
